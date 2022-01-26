3bb14a0c7d7e78c972ff8bec7dbe805e4b593f30
mustafabulu <you@example.com>
2022-01-26 05:40:21 +0300

    Enum

---
+++ src/main/java/com/patika/tutorial/EEnumColors.java
@@ -0,0 +1,43 @@
+package com.patika.tutorial;
+
+import lombok.Getter;
+import lombok.Setter;
+
+//type safe
+public enum EEnumColors {
+    BLUE,GREEN,ORANGE
+}
+
+enum Colors2{
+    BLUE(1,"blue"),GREEN(2,"green"),ORANGE(3,"orange");
+
+    @Getter @Setter
+    private final int colorid;
+    @Getter @Setter
+    private final String colorName;
+
+    private Colors2(int colorid, String colorName){
+        this.colorid=colorid;
+        this.colorName=colorName;
+
+
+    }
+}
+
+
+class Main{
+    public static void main(String[] args) {
+        EEnumColors colors=EEnumColors.BLUE;
+        System.out.println(colors);
+
+        String color2=EEnumColors.BLUE.toString();
+        System.out.println(color2.toLowerCase());
+        System.out.println("------------------------------");
+
+        String  cons=Colors2.BLUE.getColorName();
+        System.out.println(cons);
+
+
+    }
+
+}
